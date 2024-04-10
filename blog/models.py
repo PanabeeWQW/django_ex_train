ce70cdf4c7307423080248b9c43f05477080c4ee
PanabeeWQW <timofei.davydo@mail.ru>
2024-04-10 15:15:13 +0300

    Добавленна функция для возвращении имени продукта для админки

--- blog/models.py
+++ blog/models.py
@@ -3,16 +3,55 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+from django.db import models
+from django.contrib.auth.models import User
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.title
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Product(models.Model):
+    image = models.ImageField(verbose_name='Картинка товара', upload_to="products_image")
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('Новый', 'Новый'),
+        ('Подтвержденный', 'Подтвержденный'),
+        ('Закрытый', 'Закрытый')
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='New')
+    created_at = models.DateTimeField(auto_now_add=True)
+    cancellation_reason = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзина'
+
