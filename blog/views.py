233873449b1fc1ab56f0844e6863f0ac77843748
PanabeeWQW <timofei.davydo@mail.ru>
2024-04-10 15:21:11 +0300

    Переход от HttpResponse к Render

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def main(request):
+    products = Product.objects.all()
+    return render(request, 'blog/index.html', {'products': products})
+
+def about(request):
+    return render(request, 'blog/about.html')
