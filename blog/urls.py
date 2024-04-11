72fc13dd4e01461e5b8a17cec9ecb13a81fd3702
PanabeeWQW <timofei.davydo@mail.ru>
2024-04-11 15:18:07 +0300

    Доработанная главная страница

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', main, name='home'),
     path('about/', about, name='about'),
     path('find_us/', findus, name='find_us'),
+    path('product/<int:product_id>/', product, name='product')
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,6 @@ def about(request):
 
 def findus(request):
     return render(request, 'blog/findus.html')
+
+def product(request, product_id):
+    return HttpResponse(f"Название продукта {product_id}")
