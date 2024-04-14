396ca48f2750bd515e0267c6df120d668b191f0f
PanabeeWQW <timofei.davydo@mail.ru>
2024-04-14 13:00:01 +0300

    Измененны функции для каталога

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Product
+from .models import *
 
 def main(request):
     products = Product.objects.all()
@@ -14,7 +14,22 @@ def findus(request):
     return render(request, 'blog/findus.html')
 
 def product(request, product_id):
-    return HttpResponse(f"Название продукта {product_id}")
+    product = Product.objects.get(pk=product_id)  # Получаем объект товара по его идентификатору
+    return render(request, 'blog/product.html', {'product': product})
 
-def catalog(request):
-    return render(request, 'blog/catalog.html')
+def catalog(request, cat_id=None):
+    cats = Category.objects.all()
+    if cat_id is not None:
+        category = Category.objects.get(pk=cat_id)
+        products = Product.objects.filter(category=category)
+    else:
+        category = None
+        products = Product.objects.all()
+    return render(request, 'blog/catalog.html', {'category': category, 'products': products, 'cats': cats})
+
+def category(request, cat_id):
+    all_products = Product.objects.all()
+    category = Category.objects.get(pk=cat_id)
+    products = Product.objects.filter(category=category)
+    cats = Category.objects.all()
+    return render(request, 'blog/catalog.html', {'all_products': all_products, 'products': products, 'category': category, 'cats': cats})
