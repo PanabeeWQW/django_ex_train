a20977169a24e2868f06c84d7d4093373fef26ff
PanabeeWQW <timofei.davydo@mail.ru>
2024-04-14 12:59:27 +0300

    Добавление маршрутизации для каталога

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('find_us/', findus, name='find_us'),
     path('product/<int:product_id>/', product, name='product'),
+    path('category/<int:cat_id>/', category, name='category'),
     path('catalog/', catalog, name='catalog'),
 ]
